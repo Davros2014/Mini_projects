3ac2db9c09145d31a1d5ff2e60319078c6e88641
David Parsons <info@dpdesigns.eu>
2019-09-18 23:38:13 +0200

    added new images to carousel

---
+++ Image_carousel/script.js
@@ -0,0 +1,70 @@
+(function() {
+    var images = document.getElementsByClassName("images"),
+        dots = document.getElementsByClassName("dot"),
+        current = 0,
+        timer,
+        transitionComplete = true;
+
+    // holds opening image on screen for 2s before first transition
+    setTimeout(moveImages, 2000);
+
+    document.addEventListener("transitionend", function(e) {
+        // transitionend event occurs when a CSS transition has completed
+        // webkitTransitionEnd for Safari 3.1 to 6.0
+        if (e.target.classList.contains("exit")) {
+            e.target.classList.remove("exit");
+
+            console.log(
+                "Transition complete - the current image is " + current
+            );
+            timer = setTimeout(moveImages, 4000);
+            transitionComplete = false;
+        }
+    });
+
+    // iife starts loop through dots automatically
+    for (var i = 0; i < dots.length; i++) {
+        (function(i) {
+            dots[i].addEventListener("click", function(e) {
+                if (current == i) {
+                    e.target.classList.remove("on");
+                    return;
+                }
+                if (transitionComplete) {
+                    return;
+                }
+                // cancels scheduled incoming image
+                clearTimeout(timer);
+                moveImages(i);
+            });
+        })(i);
+    }
+
+    // declare function to move images and navigation dots
+    function moveImages(next) {
+        console.log("the current image is " + current);
+        images[current].classList.remove("onscreen");
+        // removes onscreen class to current image class
+        dots[current].classList.remove("on");
+        // removes on class from current dot class
+        images[current].classList.add("exit");
+        // add exit class to current image
+
+        transitionComplete = true;
+
+        current++; // current start at zero > 1 is added on each pass through
+        if (current >= images.length) {
+            // resets image number back to zero if current exceeds number of images!
+            current = 0;
+        }
+        // if next is greater than or equal to zero, assign the value of next to current
+        if (next >= 0) {
+            current = next;
+        }
+        dots[current].classList.add("on");
+        // adds on class to the next dot class
+        images[current].classList.add("onscreen");
+        console.log("The next image is " + current);
+        // adds onscreen class to the next one
+    }
+})(); // end iife
